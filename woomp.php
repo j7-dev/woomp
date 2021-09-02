1d5c4eb7879513f0ce8e013d47b4263619318c0a
Oberon Lai <m615926@gmail.com>
2021-09-02 12:37:23 +0800

    引入 appsero 套件

--- woomp.php
+++ woomp.php
@@ -56,7 +56,7 @@ define( 'WOOMP_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'WOOMP_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'WOOMP_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
 
-require WOOMP_PLUGIN_DIR . '/vendor/autoload.php';
+require WOOMP_PLUGIN_DIR . 'vendor/autoload.php';
 
 /**
  * The code that runs during plugin activation.
