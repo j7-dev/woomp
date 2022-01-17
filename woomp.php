0249eaf5fc6b13b68df3ad8d0c4d61106e740c8d
oberonlai <m615926@gmail.com>
2022-01-17 13:09:52 +0800

    新增 - 設定項加入姓名電話格式驗證開關

--- woomp.php
+++ woomp.php
@@ -2,14 +2,14 @@
 
  /**
   * @link              https://morepower.club
-  * @since             1.5.2
+  * @since             1.5.3
   * @package           woomp
   *
   * @wordpress-plugin
   * Plugin Name:       好用版擴充 MorePower Addon for WooCommerce
   * Plugin URI:        https://morepower.club/morepower-addon/
   * Description:       WooCommerce 好用版擴充，改善結帳流程與可變商品等區塊，讓 WooCommerce 更符合亞洲人使用習慣。
-  * Version:           1.5.2
+  * Version:           1.5.3
   * Author:            MorePower
   * Author URI:        https://morepower.club
   * License:           GPL-2.0+
@@ -51,7 +51,7 @@ if ( ! in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins',
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'WOOMP_VERSION', '1.5.2' );
+define( 'WOOMP_VERSION', '1.5.3' );
 define( 'WOOMP_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'WOOMP_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'WOOMP_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
