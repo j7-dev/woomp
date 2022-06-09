5934090f267b2aa495480bb3dde74c2275e99a49
oberonlai <m615926@gmail.com>
2022-06-09 11:09:31 +0800

    除錯 - 單一費率運送類別無效

--- admin/class-woomp-shipping-flat-rate.php
+++ admin/class-woomp-shipping-flat-rate.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * 複寫單一費率的設定項
+ * 複寫單一費率的設定項以增加免運費功能
  */
 
 use Automattic\WooCommerce\Utilities\NumberUtil;
@@ -225,6 +225,43 @@ class WooMP_Shipping_Flat_Rate extends WC_Shipping_Flat_Rate {
 				'no_count' => 1,
 			),
 		);
+
+		// Add shipping class costs.
+		$shipping_classes = WC()->shipping()->get_shipping_classes();
+
+		if ( ! empty( $shipping_classes ) ) {
+			$found_shipping_classes = $this->find_shipping_classes( $package );
+			$highest_class_cost     = 0;
+
+			foreach ( $found_shipping_classes as $shipping_class => $products ) {
+				// Also handles BW compatibility when slugs were used instead of ids.
+				$shipping_class_term = get_term_by( 'slug', $shipping_class, 'product_shipping_class' );
+				$class_cost_string   = $shipping_class_term && $shipping_class_term->term_id ? $this->get_option( 'class_cost_' . $shipping_class_term->term_id, $this->get_option( 'class_cost_' . $shipping_class, '' ) ) : $this->get_option( 'no_class_cost', '' );
+
+				if ( '' === $class_cost_string ) {
+					continue;
+				}
+
+				$has_costs  = true;
+				$class_cost = $this->evaluate_cost(
+					$class_cost_string,
+					array(
+						'qty'  => array_sum( wp_list_pluck( $products, 'quantity' ) ),
+						'cost' => array_sum( wp_list_pluck( $products, 'line_total' ) ),
+					)
+				);
+
+				if ( 'class' === $this->type ) {
+					$rate['cost'] += $class_cost;
+				} else {
+					$highest_class_cost = $class_cost > $highest_class_cost ? $class_cost : $highest_class_cost;
+				}
+			}
+
+			if ( 'order' === $this->type && $highest_class_cost ) {
+				$rate['cost'] += $highest_class_cost;
+			}
+		}
 
 		$has_coupon     = $this->check_has_coupon( $this->cost_requires, array( 'coupon', 'min_amount_or_coupon', 'min_amount_and_coupon' ) );
 		$has_min_amount = $this->check_has_min_amount( $this->cost_requires, array( 'min_amount', 'min_amount_or_coupon', 'min_amount_and_coupon' ) );
