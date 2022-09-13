74eb1cbf869e7b848a06a6f614e1fbbd45ba0f7f
Tunglin <q51605@gmail.com>
2022-09-13 15:43:11 +0800

    除錯 - 修正刪除購物車商品，結帳頁面發生錯誤

---
+++ woocommerce/checkout/cart-errors.php
@@ -0,0 +1,11 @@
+<?php
+defined('ABSPATH') || exit;
+?>
+
+<script>
+    jQuery(document).ready(function(){
+        if(!window.location.hash) {
+            window.location.reload();
+        }
+    });
+</script>
