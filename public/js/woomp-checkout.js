1332044671a563ecd7cfbde205253f5ed34674d4
oberonlai <m615926@gmail.com>
2022-10-03 12:27:55 +0800

    除錯 - 結帳頁帳單的地址欄位多一個空格

--- public/js/woomp-checkout.js
+++ public/js/woomp-checkout.js
@@ -209,12 +209,10 @@ jQuery(function($){
 	// 勾選離島運送選項
 	function setIslandShipping( status ){
 		if( status === 'show' ){
-			if( $('input[name="billing_address_1"]').val() === '' ){
-				$('input[name="billing_address_1"]').val(' ');
-			}
 			$('#billing_island_field').show();
 			$('#billing_island_field').prependTo('#order_review');
 			$('#billing_island').on('change', function(){
+				$(document.body).trigger('update_checkout');
 				island = ['金門縣','澎湖縣','連江縣']
 				islandHide = $('#billing_island_none').val().split(',');
 				countyHide = ['基隆市','臺北市','新北市','宜蘭縣','新竹市','新竹縣','桃園市','苗栗縣','臺中市','彰化縣','南投縣','嘉義市','嘉義縣','雲林縣','臺南市','高雄市','屏東縣','臺東縣','花蓮縣']
