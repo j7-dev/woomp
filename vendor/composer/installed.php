a96d46a3d04bf00d14138c79d8d1742177629481
oberonlai <m615926@gmail.com>
2023-06-16 17:21:33 +0800

    新增 - 更新伺服器 SDK

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '283576f7ad8bd521806d41ac43cec8812a86b5c3',
+        'reference' => 'f79e16ecda47058e793d0af5002af39271ffb71f',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '283576f7ad8bd521806d41ac43cec8812a86b5c3',
+            'reference' => 'f79e16ecda47058e793d0af5002af39271ffb71f',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
