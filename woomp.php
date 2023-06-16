d2cecb44f0c9e2ea6e8f2fae9f243a14f27dfbb5
oberonlai <m615926@gmail.com>
2023-06-16 18:19:17 +0800

    新增 - 更新伺服器 SDK

--- woomp.php
+++ woomp.php
@@ -62,7 +62,7 @@ require WOOMP_PLUGIN_DIR . 'vendor/autoload.php';
 /**
  * Appsero Update
  */
-$client = new Appsero\Client( '95f8607c-8835-49e9-8e7c-4e4f4da1f461', '好用版擴充 MorePower Addon for WooCommerce', __FILE__ );
+$client = new Appsero\Client( 'cfefd42a-c007-4ef7-8112-58ff5b1d2a8b', '好用版擴充 MorePower Addon for WooCommerce', __FILE__ );
 $client->insights()->init();
 $client->updater();
 
