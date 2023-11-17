a913481e64bbf91a50d65dcc5387125db2931675
J7 <j7.dev.gg@outlook.com>
2023-11-17 11:41:22 +0900

    feat: test version

--- woomp.php
+++ woomp.php
@@ -4,8 +4,8 @@
  * @wordpress-plugin
  * Plugin Name:       好用版擴充 MorePower Addon for WooCommerce
  * Plugin URI:        https://morepower.club/morepower-addon/
- * Description:       WooCommerce 好用版擴充，改善結帳流程與可變商品等區塊，讓 WooCommerce 更符合亞洲人使用習慣。
- * Version:           3.2.6
+ * Description:       WooCommerce 好用版擴充，改善結帳流程與可變商品等區塊，並整合多項金流，讓 WooCommerce 更符合亞洲人使用習慣。
+ * Version:           3.2.7
  * Author:            MorePower
  * Author URI:        https://morepower.club
  * License:           GPL-2.0+
