268e91107ef8626f26e5d9345deb9f2a54cc806f
J7 <j7.dev.gg@outlook.com>
2023-12-21 12:21:30 +0800

    fix: [統一金] 啟用 3D 驗證後重複授權的問題 #31

--- includes/payuni/src/gateways/CreditSubscription.php
+++ includes/payuni/src/gateways/CreditSubscription.php
@@ -18,194 +18,193 @@ defined('ABSPATH') || exit;
 class CreditSubscription extends AbstractGateway
 {
 
-	/**
-	 * Constructor
-	 */
-	public function __construct()
-	{
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
 
-		parent::__construct();
+        parent::__construct();
 
-		$this->plugin_name = 'payuni-payment-credit-subscription';
-		$this->version     = '1.0.0';
-		$this->has_fields  = true;
-		// $this->order_button_text = __( '統一金流 PAYUNi 信用卡', 'woomp' );
+        $this->plugin_name = 'payuni-payment-credit-subscription';
+        $this->version     = '1.0.0';
+        $this->has_fields  = true;
+        // $this->order_button_text = __( '統一金流 PAYUNi 信用卡', 'woomp' );
 
-		$this->id                 = 'payuni-credit-subscription';
-		$this->method_title       = __('統一金流 PAYUNi 信用卡定期定額', 'woomp');
-		$this->method_description = __('透過統一金流 PAYUNi 信用卡定期定額付款', 'woomp');
+        $this->id                 = 'payuni-credit-subscription';
+        $this->method_title       = __('統一金流 PAYUNi 信用卡定期定額', 'woomp');
+        $this->method_description = __('透過統一金流 PAYUNi 信用卡定期定額付款', 'woomp');
 
-		$this->init_form_fields();
-		$this->init_settings();
+        $this->init_form_fields();
+        $this->init_settings();
 
-		$this->title            = $this->get_option('title');
-		$this->description      = $this->get_option('description');
-		$this->supports         = array(
-			'products',
-			'subscriptions',
-			'subscription_cancellation',
-			'subscription_suspension',
-			'subscription_reactivation',
-			'subscription_amount_changes',
-			'subscription_date_changes',
-			'subscription_payment_method_change',
-			'subscription_payment_method_change_customer',
-			'subscription_payment_method_change_admin',
-			'multiple_subscriptions',
-			'tokenization',
-		);
-		$this->api_endpoint_url = 'api/credit';
+        $this->title       = $this->get_option('title');
+        $this->description = $this->get_option('description');
+        $this->supports    = array(
+            'products',
+            'subscriptions',
+            'subscription_cancellation',
+            'subscription_suspension',
+            'subscription_reactivation',
+            'subscription_amount_changes',
+            'subscription_date_changes',
+            'subscription_payment_method_change',
+            'subscription_payment_method_change_customer',
+            'subscription_payment_method_change_admin',
+            'multiple_subscriptions',
+            'tokenization',
+        );
+        $this->api_endpoint_url = 'api/credit';
 
-		add_action(
-			'woocommerce_update_options_payment_gateways_' . $this->id,
-			array(
-				$this,
-				'process_admin_options',
-			)
-		);
+        add_action(
+            'woocommerce_update_options_payment_gateways_' . $this->id,
+            array(
+                $this,
+                'process_admin_options',
+            )
+        );
 
-		add_filter('payuni_transaction_args_' . $this->id, array($this, 'add_args'), 10, 3);
-	}
+        add_filter('payuni_transaction_args_' . $this->id, array($this, 'add_args'), 10, 3);
+    }
 
-	/**
-	 * Setup form fields for payment
-	 *
-	 * @return void
-	 */
-	public function init_form_fields()
-	{
-		$this->form_fields = array(
-			'enabled'     => array(
-				'title'   => __('Enable/Disable', 'woocommerce'),
-				'type'    => 'checkbox',
-				/* translators: %s: Gateway method title */
-				'label'   => sprintf(__('Enable %s', 'woomp'), $this->method_title),
-				'default' => 'no',
-			),
-			'title'       => array(
-				'title'       => __('Title', 'woocommerce'),
-				'type'        => 'text',
-				'default'     => $this->method_title,
-				'description' => __('This controls the title which the user sees during checkout.', 'woocommerce'),
-				'desc_tip'    => true,
-			),
-			'description' => array(
-				'title'       => __('Description', 'woocommerce'),
-				'type'        => 'textarea',
-				'css'         => 'width: 400px;',
-				'default'     => $this->order_button_text,
-				'description' => __('This controls the description which the user sees during checkout.', 'woocommerce'),
-				'desc_tip'    => true,
-			),
-		);
-	}
+    /**
+     * Setup form fields for payment
+     *
+     * @return void
+     */
+    public function init_form_fields()
+    {
+        $this->form_fields = array(
+            'enabled'     => array(
+                'title'   => __('Enable/Disable', 'woocommerce'),
+                'type'    => 'checkbox',
+                /* translators: %s: Gateway method title */
+                'label'   => sprintf(__('Enable %s', 'woomp'), $this->method_title),
+                'default' => 'no',
+            ),
+            'title'       => array(
+                'title'       => __('Title', 'woocommerce'),
+                'type'        => 'text',
+                'default'     => $this->method_title,
+                'description' => __('This controls the title which the user sees during checkout.', 'woocommerce'),
+                'desc_tip'    => true,
+            ),
+            'description' => array(
+                'title'       => __('Description', 'woocommerce'),
+                'type'        => 'textarea',
+                'css'         => 'width: 400px;',
+                'default'     => $this->order_button_text,
+                'description' => __('This controls the description which the user sees during checkout.', 'woocommerce'),
+                'desc_tip'    => true,
+            ),
+        );
+    }
 
-	/**
-	 * Filter payment api arguments for atm
-	 *
-	 * @param array    $args  The payment api arguments.
-	 * @param WC_Order $order The order object.
-	 *
-	 * @return array
-	 */
-	public function add_args(array $args, WC_Order $order, array $card_data): array
-	{
+    /**
+     * Filter payment api arguments for atm
+     *
+     * @param array    $args  The payment api arguments.
+     * @param WC_Order $order The order object.
+     *
+     * @return array
+     */
+    public function add_args(array $args, WC_Order $order, array $card_data): array
+    {
 
-		try {
-			unset($data['CardNo']);
-			unset($data['CardExpired']);
-			unset($data['CardCVC']);
+        try {
+            unset($data[ 'CardNo' ]);
+            unset($data[ 'CardExpired' ]);
+            unset($data[ 'CardCVC' ]);
 
-			$subscriptions = \wcs_get_subscriptions_for_order($order);
-			$TradeAmt = 0;
-			foreach ($subscriptions as $subscription) {
-				$end_date = $subscription->get_date('trial_end');
-				$end_date_timestamp = strtotime($end_date);
-				$current = time();
-				$is_trial =  $end_date_timestamp > $current;
+            $subscriptions = \wcs_get_subscriptions_for_order($order);
+            $TradeAmt      = 0;
+            foreach ($subscriptions as $subscription) {
+                $end_date           = $subscription->get_date('trial_end');
+                $end_date_timestamp = strtotime($end_date);
+                $current            = time();
+                $is_trial           = $end_date_timestamp > $current;
 
-				// 如果還在試用期，就扣5元，如果不是就是總金額
-				// 用 order total 會有問題，因為 order total 會是第一期的金額
-				$TradeAmt += $is_trial ? 5 : $order->get_total();
-			}
+                // 如果還在試用期，就扣5元，如果不是就是總金額
+                // 用 order total 會有問題，因為 order total 會是第一期的金額
+                $TradeAmt += $is_trial ? 5 : $order->get_total();
+            }
 
-			$data = array(
-				'TradeAmt' => $TradeAmt,
-				'CreditToken' => $order->get_billing_email()
-			);
+            $data = array(
+                'TradeAmt'    => $TradeAmt,
+                'CreditToken' => $order->get_billing_email(),
+            );
 
-			if (wc_string_to_bool(get_option('payuni_3d_auth'))) {
-				$data['API3D']     = 1;
-				$data['NotifyURL'] = home_url('wc-api/payuni_notify_card');
-				$data['ReturnURL'] = home_url('wc-api/payuni_notify_card');
-			}
+            if (wc_string_to_bool(get_option('payuni_3d_auth'))) {
+                $data[ 'API3D' ] = 1;
+                // $data[ 'NotifyURL' ] = home_url('wc-api/payuni_notify_card');
+                $data[ 'ReturnURL' ] = home_url('wc-api/payuni_notify_card');
+            }
 
+            if (!empty($card_data)) {
+                if (!empty($card_data[ 'token_id' ]) && 'new' !== $card_data[ 'token_id' ]) {
+                    $token                = \WC_Payment_Tokens::get($card_data[ 'token_id' ]);
+                    $data[ 'CreditHash' ] = $token->get_token();
+                }
+            }
+            return array_merge(
+                $args,
+                $data
+            );
+        } catch (\Throwable $th) {
+            \WC_Log_Handler_DB::add(time(), 'error', 'woomp debug', 'woomp', $th);
+        }
+    }
 
-			if (!empty($card_data)) {
-				if (!empty($card_data['token_id']) && 'new' !== $card_data['token_id']) {
-					$token              = \WC_Payment_Tokens::get($card_data['token_id']);
-					$data['CreditHash'] = $token->get_token();
-				}
-			}
-			return array_merge(
-				$args,
-				$data
-			);
-		} catch (\Throwable $th) {
-			\WC_Log_Handler_DB::add(time(), 'error', 'woomp debug', 'woomp', $th);
-		}
-	}
+    /**
+     * Process payment
+     *
+     * @param string $order_id The order id.
+     *
+     * @return array
+     */
+    public function process_payment($order_id): array
+    {
 
-	/**
-	 * Process payment
-	 *
-	 * @param string $order_id The order id.
-	 *
-	 * @return array
-	 */
-	public function process_payment($order_id): array
-	{
+        $order = new WC_Order($order_id);
 
-		$order = new WC_Order($order_id);
+        //@codingStandardsIgnoreStart
+        $number   = (isset($_POST[ $this->id . '-card-number' ])) ? wc_clean(wp_unslash($_POST[ $this->id . '-card-number' ])) : '';
+        $expiry   = (isset($_POST[ $this->id . '-card-expiry' ])) ? wc_clean(wp_unslash(str_replace(' ', '', $_POST[ $this->id . '-card-expiry' ]))) : '';
+        $cvc      = (isset($_POST[ $this->id . '-card-cvc' ])) ? wc_clean(wp_unslash($_POST[ $this->id . '-card-cvc' ])) : '';
+        $token_id = (isset($_POST[ 'wc-' . $this->id . '-payment-token' ])) ? wc_clean(wp_unslash($_POST[ 'wc-' . $this->id . '-payment-token' ])) : '';
+        $new      = (isset($_POST[ 'wc-' . $this->id . '-new-payment-method' ])) ? wc_clean(wp_unslash($_POST[ 'wc-' . $this->id . '-new-payment-method' ])) : '';
+        //@codingStandardsIgnoreEnd
 
-		//@codingStandardsIgnoreStart
-		$number   = (isset($_POST[$this->id . '-card-number'])) ? wc_clean(wp_unslash($_POST[$this->id . '-card-number'])) : '';
-		$expiry   = (isset($_POST[$this->id . '-card-expiry'])) ? wc_clean(wp_unslash(str_replace(' ', '', $_POST[$this->id . '-card-expiry']))) : '';
-		$cvc      = (isset($_POST[$this->id . '-card-cvc'])) ? wc_clean(wp_unslash($_POST[$this->id . '-card-cvc'])) : '';
-		$token_id = (isset($_POST['wc-' . $this->id . '-payment-token'])) ? wc_clean(wp_unslash($_POST['wc-' . $this->id . '-payment-token'])) : '';
-		$new      = (isset($_POST['wc-' . $this->id . '-new-payment-method'])) ? wc_clean(wp_unslash($_POST['wc-' . $this->id . '-new-payment-method'])) : '';
-		//@codingStandardsIgnoreEnd
+        $card_data = array(
+            'number'   => str_replace(' ', '', $number),
+            'expiry'   => str_replace('/', '', $expiry),
+            'cvc'      => $cvc,
+            'token_id' => $token_id,
+            'new'      => $new,
+        );
 
-		$card_data = array(
-			'number'   => str_replace(' ', '', $number),
-			'expiry'   => str_replace('/', '', $expiry),
-			'cvc'      => $cvc,
-			'token_id' => $token_id,
-			'new'      => $new,
-		);
+        $request = new Request(new self());
 
-		$request = new Request(new self());
+        return $request->build_request($order, $card_data);
+    }
 
-		return $request->build_request($order, $card_data);
-	}
+    /**
+     * Display payment detail after order table
+     *
+     * @param WC_Order $order The order object.
+     *
+     * @return void
+     */
+    public function get_detail_after_order_table($order)
+    {
+        if ($order->get_payment_method() === $this->id) {
 
-	/**
-	 * Display payment detail after order table
-	 *
-	 * @param WC_Order $order The order object.
-	 *
-	 * @return void
-	 */
-	public function get_detail_after_order_table($order)
-	{
-		if ($order->get_payment_method() === $this->id) {
+            $status   = $order->get_meta('_payuni_resp_status', true);
+            $message  = $order->get_meta('_payuni_resp_message', true);
+            $trade_no = $order->get_meta('_payuni_resp_trade_no', true);
+            $card_4no = $order->get_meta('_payuni_card_number', true);
 
-			$status   = $order->get_meta('_payuni_resp_status', true);
-			$message  = $order->get_meta('_payuni_resp_message', true);
-			$trade_no = $order->get_meta('_payuni_resp_trade_no', true);
-			$card_4no = $order->get_meta('_payuni_card_number', true);
-
-			$html = '
+            $html = '
 				<h2 class="woocommerce-order-details__title">交易明細</h2>
 				<div class="responsive-table">
 					<table class="woocommerce-table woocommerce-table--order-details shop_table order_details">
@@ -230,7 +229,7 @@ class CreditSubscription extends AbstractGateway
 					</table>
 				</div>
 			';
-			echo $html;
-		}
-	}
+            echo $html;
+        }
+    }
 }
