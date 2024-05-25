222d69239f77ce9125f811c4ef9415ec0190043d
J7 <j7.dev.gg@outlook.com>
2024-05-25 15:54:06 +0800

    fix: 打折導致金額不同，綠界電子發票開立失敗 #45

--- includes/woomp-ecpay-invoice/src/apis/EcpayInvoiceHandler.php
+++ includes/woomp-ecpay-invoice/src/apis/EcpayInvoiceHandler.php
@@ -4,93 +4,92 @@ namespace WOOMPECPAYINVOICE\APIs;
 
 use WOOMPECPAYINVOICE\APIs\EcpayInvoiceFiels;
 
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
 
-class EcpayInvoiceHandler
-{
+class EcpayInvoiceHandler {
+
 
 	/**
 	 * 開立發票
 	 */
-	public function generate_invoice($order_id)
-	{
+	public function generate_invoice( $order_id ) {
 
-		$order       = wc_get_order($order_id);
+		$order       = wc_get_order( $order_id );
 		$order_total = $order->get_total();
 		$order_info  = $order->get_address();
 
-		if ('0' === $order_total) {
+		if ( '0' === $order_total ) {
 			return;
 		}
 
 		// 訂購人資料.
 		$customerName = $order_info['last_name'] . $order_info['first_name'];
 		$orderEmail   = $order_info['email'];
-		$orderPhone   = str_replace('+886', '0', $order_info['phone']);
+		$orderPhone   = str_replace( '+886', '0', $order_info['phone'] );
 		$orderAddress = $order_info['country'] . $order_info['state'] . $order_info['city'] . $order_info['address_1'] . $order_info['address_2'];
 
-		$customerIdentifier = EcpayInvoiceFields::get_meta($order_id, 'tax_id'); // 統一編號
-		$invoiceType        = EcpayInvoiceFields::get_meta($order_id, 'type');
+		$customerIdentifier = EcpayInvoiceFields::get_meta( $order_id, 'tax_id' ); // 統一編號
+		$invoiceType        = EcpayInvoiceFields::get_meta( $order_id, 'type' );
 
-		$donation = ($invoiceType == 'donate') ? 1 : 0; // 捐贈
-		$donation = (empty($customerIdentifier)) ? $donation : 0; // 如果有寫統一發票號碼則無法捐贈
+		$donation = ( $invoiceType == 'donate' ) ? 1 : 0; // 捐贈
+		$donation = ( empty( $customerIdentifier ) ) ? $donation : 0; // 如果有寫統一發票號碼則無法捐贈
 		$print    = 0;
 
 		// 有打統一編號 強制列印.
-		if (!empty($customerIdentifier)) {
+		if ( ! empty( $customerIdentifier ) ) {
 			$print = 1;
 
 			// 有統一編號 則取得公司名稱.
-			$sCompany_Name = EcpayInvoiceFields::get_meta($order_id, 'company_name'); // 公司名稱
-			$customerName  = (!empty($sCompany_Name)) ? $sCompany_Name : $customerName;
+			$sCompany_Name = EcpayInvoiceFields::get_meta( $order_id, 'company_name' ); // 公司名稱
+			$customerName  = ( ! empty( $sCompany_Name ) ) ? $sCompany_Name : $customerName;
 		}
 
-		$loveCode    = EcpayInvoiceFields::get_meta($order_id, 'donate'); // 捐贈碼
-		$carruerType = EcpayInvoiceFields::get_meta($order_id, 'individual'); // 載具
-		$carruerType = ($carruerType == 0) ? '' : $carruerType;
+		$loveCode    = EcpayInvoiceFields::get_meta( $order_id, 'donate' ); // 捐贈碼
+		$carruerType = EcpayInvoiceFields::get_meta( $order_id, 'individual' ); // 載具
+		$carruerType = ( $carruerType == 0 ) ? '' : $carruerType;
 
 		// 無載具 強制列印
-		if (empty($carruerType)) {
+		if ( empty( $carruerType ) ) {
 			$print = 1;
 		}
 
 		// 有捐贈項目 不允許列印
-		if ($donation == 1) {
+		if ( $donation == 1 ) {
 			$print = 0;
 		}
 
 		$carruerNum = '';
-		if (2 === $carruerType || 3 === $carruerType) {
-			$carruerNum = EcpayInvoiceFields::get_meta($order_id, 'carrier'); // 載具編號
+		if ( 2 === $carruerType || 3 === $carruerType ) {
+			$carruerNum = EcpayInvoiceFields::get_meta( $order_id, 'carrier' ); // 載具編號
 		}
 
 		// 付款成功次數 第一次付款或沒有此欄位則設定為空值
 		$totalSuccessTimes = '';
-		if (isset($order_info['_total_success_times'][0])) {
-			$totalSuccessTimes = ($orderInfo['_total_success_times'][0] == '') ? '' : $orderInfo['_total_success_times'][0];
+		if ( isset( $order_info['_total_success_times'][0] ) ) {
+			$totalSuccessTimes = ( $orderInfo['_total_success_times'][0] == '' ) ? '' : $orderInfo['_total_success_times'][0];
 		}
 
 		$invoiceRemark = '';
 
 		// 有打統一編號 強制列印發票
-		if (!empty($customerIdentifier)) {
+		if ( ! empty( $customerIdentifier ) ) {
 
 			$print = 1;
 
 			// // 有統一編號 則取得公司名稱
-			$sCompany_Name = EcpayInvoiceFields::get_meta($order_id, 'company_name'); // 公司名稱
-			$customerName  = (!empty($sCompany_Name)) ? $sCompany_Name : $customerName;
+			$sCompany_Name = EcpayInvoiceFields::get_meta( $order_id, 'company_name' ); // 公司名稱
+			$customerName  = ( ! empty( $sCompany_Name ) ) ? $sCompany_Name : $customerName;
 			$carruerType   = $carruerNum = '';
 		}
 
 		// 無載具 強制列印
-		if (empty($carruerType)) {
+		if ( empty( $carruerType ) ) {
 
 			$print = 1;
 		}
 
 		// 有捐贈項目 不允許列印
-		if ($donation == 1) {
+		if ( $donation == 1 ) {
 
 			$print = 0;
 		}
@@ -107,43 +106,42 @@ class EcpayInvoiceHandler
 			$ecpay_invoice->MerchantID     = $this->get_api_key()['merchant_id'];
 			$ecpay_invoice->HashKey        = $this->get_api_key()['hashkey'];
 			$ecpay_invoice->HashIV         = $this->get_api_key()['hashiv'];
-			$ecpay_invoice->Send['Items']  = [];
+			$ecpay_invoice->Send['Items']  = array();
 
 			// 取得商品資訊
-			$items    = [];
+			$items    = array();
 			$itemsTmp = $order->get_items();
 
-
-
-
 			// 商品資訊
-			foreach ($itemsTmp as $key => $WC_Order_Item_Product) {
-				$items[$key]['ItemName']   = wp_strip_all_tags(str_replace('|', '-', $WC_Order_Item_Product->get_name()), true); // 商品名稱 ItemName
-				$items[$key]['ItemCount']  = $WC_Order_Item_Product->get_quantity(); // 數量 ItemCount
-				$items[$key]['ItemAmount'] = round((float) $WC_Order_Item_Product->get_total() + (float) $WC_Order_Item_Product->get_total_tax(), 2); // 小計 ItemAmount
-				$items[$key]['ItemPrice']  = round((float) $items[$key]['ItemAmount'] / (float) $items[$key]['ItemCount'], 2); // 單價 ItemPrice
+			$order_total_summed_by_items = 0;
+			foreach ( $itemsTmp as $key => $WC_Order_Item_Product ) {
+				$items[ $key ]['ItemName']    = wp_strip_all_tags( str_replace( '|', '-', $WC_Order_Item_Product->get_name() ), true ); // 商品名稱 ItemName
+				$items[ $key ]['ItemCount']   = $WC_Order_Item_Product->get_quantity(); // 數量 ItemCount
+				$items[ $key ]['ItemAmount']  = round( (float) $WC_Order_Item_Product->get_total() + (float) $WC_Order_Item_Product->get_total_tax(), 2 ); // 小計 ItemAmount
+				$items[ $key ]['ItemPrice']   = round( (float) $items[ $key ]['ItemAmount'] / (float) $items[ $key ]['ItemCount'], 2 ); // 單價 ItemPrice
+				$order_total_summed_by_items += $items[ $key ]['ItemAmount']; // 將 items 總額加總起來
 			}
 
 			// 組合商品
-			foreach ($items as $key => $value) {
+			foreach ( $items as $key => $value ) {
 
 				array_push(
 					$ecpay_invoice->Send['Items'],
 					array(
-						'ItemName'    => str_replace('|', '-', $value['ItemName']),
+						'ItemName'    => str_replace( '|', '-', $value['ItemName'] ),
 						'ItemCount'   => $value['ItemCount'],
 						'ItemWord'    => '批',
-						'ItemPrice'   => round((float) $value['ItemPrice'], 2),
+						'ItemPrice'   => round( (float) $value['ItemPrice'], 2 ),
 						'ItemTaxType' => 1,
-						'ItemAmount'  => round((float) $value['ItemAmount'], 2),
+						'ItemAmount'  => round( (float) $value['ItemAmount'], 2 ),
 					)
 				);
 			}
 
 			// 運費
-			$shippingTotal = number_format((float) $order->get_total_shipping() + (float) $order->get_shipping_tax(), wc_get_price_decimals(), '.', '');
+			$shippingTotal = number_format( (float) $order->get_total_shipping() + (float) $order->get_shipping_tax(), wc_get_price_decimals(), '.', '' );
 
-			if ($shippingTotal != 0) {
+			if ( $shippingTotal != 0 ) {
 
 				array_push(
 					$ecpay_invoice->Send['Items'],
@@ -151,9 +149,9 @@ class EcpayInvoiceHandler
 						'ItemName'    => '運費',
 						'ItemCount'   => 1,
 						'ItemWord'    => '式',
-						'ItemPrice'   => round((float) $shippingTotal, 2),
+						'ItemPrice'   => round( (float) $shippingTotal, 2 ),
 						'ItemTaxType' => 1,
-						'ItemAmount'  => round((float) $shippingTotal, 2),
+						'ItemAmount'  => round( (float) $shippingTotal, 2 ),
 					)
 				);
 			}
@@ -161,27 +159,34 @@ class EcpayInvoiceHandler
 			// 取得費用
 			$fee_amount = 0;
 			$fees       = $order->get_fees();
-			foreach ($fees as $fee) {
+			foreach ( $fees as $fee ) {
 				$fee_amount += $fee->get_amount();
 			}
-			if ($fee_amount !== 0) {
+			if ( $fee_amount !== 0 ) {
 				array_push(
 					$ecpay_invoice->Send['Items'],
 					array(
 						'ItemName'    => '費用',
 						'ItemCount'   => 1,
 						'ItemWord'    => '式',
-						'ItemPrice'   => round((float) $fee_amount, 2),
+						'ItemPrice'   => round( (float) $fee_amount, 2 ),
 						'ItemTaxType' => 1,
-						'ItemAmount'  => round((float) $fee_amount, 2),
+						'ItemAmount'  => round( (float) $fee_amount, 2 ),
 					)
 				);
 			}
 
-			// 測試用使用時間標記，正式上線只能單一不重複。
-			$relateNumber = date('YmdHis');
+			$order_total_summed_by_items += round( (float) $shippingTotal, 2 ) + round( (float) $fee_amount, 2 );
 
-			$ecpay_invoice->Send['RelateNumber']       = get_option('wc_woomp_ecpay_invoice_order_prefix') . $relateNumber;
+			if ( $order_total !== $order_total_summed_by_items && ( abs( $order_total - $order_total_summed_by_items ) < 2 ) ) {
+				// 如果金額不符合，且差距在 2 元內，就是把 $order_total 改為 $order_total_summed_by_items
+				$order_total = $order_total_summed_by_items;
+			}
+
+			// 測試用使用時間標記，正式上線只能單一不重複。
+			$relateNumber = date( 'YmdHis' );
+
+			$ecpay_invoice->Send['RelateNumber']       = get_option( 'wc_woomp_ecpay_invoice_order_prefix' ) . $relateNumber;
 			$ecpay_invoice->Send['CustomerID']         = '';
 			$ecpay_invoice->Send['CustomerIdentifier'] = $customerIdentifier;
 			$ecpay_invoice->Send['CustomerName']       = $customerName;
@@ -195,7 +200,7 @@ class EcpayInvoiceHandler
 			$ecpay_invoice->Send['CarruerType']        = $carruerType;
 			$ecpay_invoice->Send['CarruerNum']         = $carruerNum;
 			$ecpay_invoice->Send['TaxType']            = 1;
-			$ecpay_invoice->Send['SalesAmount']        = round((float) $order_total);
+			$ecpay_invoice->Send['SalesAmount']        = round( (float) $order_total );
 			$ecpay_invoice->Send['InvoiceRemark']      = $invoiceRemark;
 			$ecpay_invoice->Send['InvType']            = '07';
 			$ecpay_invoice->Send['vat']                = '';
@@ -211,49 +216,46 @@ class EcpayInvoiceHandler
 			$invoice_number  = $return_info['InvoiceNumber'];
 			$invoice_message = $return_info['RtnMsg'];
 
-			$invocie_result = ($invoice_date) ? __('<b>Invoice issue result</b>', 'woomp') : __('<b>Invoice issue faild</b>', 'woomp');
+			$invocie_result = ( $invoice_date ) ? __( '<b>Invoice issue result</b>', 'woomp' ) : __( '<b>Invoice issue faild</b>', 'woomp' );
 
-			$invocie_time   = ($invoice_date) ? __('<br>Generate Time: ', 'woomp') . $invoice_date : '';
-			$invocie_number = ($invoice_date) ? __('<br>Invoice Number: ', 'woomp') . $invoice_number : '';
-			$invoice_msg    = __('<br>Invoice Message: ', 'woomp') . $invoice_message;
-			$order->add_order_note($invocie_result . $invocie_time . $invocie_number . $invoice_msg);
+			$invocie_time   = ( $invoice_date ) ? __( '<br>Generate Time: ', 'woomp' ) . $invoice_date : '';
+			$invocie_number = ( $invoice_date ) ? __( '<br>Invoice Number: ', 'woomp' ) . $invoice_number : '';
+			$invoice_msg    = __( '<br>Invoice Message: ', 'woomp' ) . $invoice_message;
+			$order->add_order_note( $invocie_result . $invocie_time . $invocie_number . $invoice_msg );
 
-			if (!empty($sMsg)) {
-				$order->add_order_note($sMsg);
+			if ( ! empty( $sMsg ) ) {
+				$order->add_order_note( $sMsg );
 			}
 
 			// 寫入發票回傳資訊
-			if (isset($return_info['RtnCode']) && $return_info['RtnCode'] == 1) {
+			if ( isset( $return_info['RtnCode'] ) && $return_info['RtnCode'] == 1 ) {
 
 				// 異動已經開立發票的狀態 1.已經開立 0.尚未開立
-				$order->update_meta_data('_ecpay_invoice_status', 1);
+				$order->update_meta_data( '_ecpay_invoice_status', 1 );
 				// 寫入發票號碼
-				$order->update_meta_data('_ecpay_invoice_number', $return_info['InvoiceNumber']);
+				$order->update_meta_data( '_ecpay_invoice_number', $return_info['InvoiceNumber'] );
 				$order->save();
 			}
 
-
-
 			return $invoice_message;
 			// return __('Generate Ecpay invoice issue finish!', 'woomp');
-		} catch (\Exception $e) {
+		} catch ( \Exception $e ) {
 			// 例外錯誤處理.
-			return new \WP_Error('error', $e->getMessage());
+			return new \WP_Error( 'error', $e->getMessage() );
 		}
 	}
 
 	/**
 	 * 作廢發票
 	 */
-	public function invalid_invoice($order_id)
-	{
+	public function invalid_invoice( $order_id ) {
 
 		global $woocommerce, $post;
 
-		$order        = new \WC_Order($order_id);
-		$orderStatus  = $order->get_status($order_id);
-		$orderInfo    = get_post_meta($order_id);
-		$relateNumber = date('YmdHis');
+		$order        = new \WC_Order( $order_id );
+		$orderStatus  = $order->get_status( $order_id );
+		$orderInfo    = get_post_meta( $order_id );
+		$relateNumber = date( 'YmdHis' );
 
 		// 付款成功最後的次 第一次付款或沒有此欄位則設定為空值
 		// $totalSuccessTimes = ( isset( $orderInfo['_total_success_times'][0] ) && $orderInfo['_total_success_times'][0] == '' ) ? '' : $orderInfo['_total_success_times'][0];
@@ -262,11 +264,11 @@ class EcpayInvoiceHandler
 		// 已經開立發票才允許(找出最後一次)
 		$_ecpay_invoice_status = '_ecpay_invoice_status' . $totalSuccessTimes;
 
-		if (isset($orderInfo[$_ecpay_invoice_status][0]) && $orderInfo[$_ecpay_invoice_status][0] == 1) {
+		if ( isset( $orderInfo[ $_ecpay_invoice_status ][0] ) && $orderInfo[ $_ecpay_invoice_status ][0] == 1 ) {
 
 			// 發票號碼
 			$_ecpay_invoice_number = '_ecpay_invoice_number' . $totalSuccessTimes;
-			$invoice_number        = get_post_meta($order_id, $_ecpay_invoice_number, true);
+			$invoice_number        = get_post_meta( $order_id, $_ecpay_invoice_number, true );
 
 			// 呼叫SDK 作廢發票
 			try {
@@ -288,7 +290,7 @@ class EcpayInvoiceHandler
 
 				// 4.送出
 				$return_info = $ecpayInvoice->Check_Out();
-			} catch (\Exception $e) {
+			} catch ( \Exception $e ) {
 
 				// 例外錯誤處理。
 				$msg = $e->getMessage();
@@ -297,16 +299,16 @@ class EcpayInvoiceHandler
 			// 於備註區寫入發票資訊
 			$invoice_number  = $return_info['InvoiceNumber'];
 			$invoice_message = $return_info['RtnMsg'];
-			$invoiceNote     = __('<b>Ecpay invalid invoice result</b>', 'woomp') . __('<br>Invoice Number: ', 'woomp') . $invoice_number . __('<br>Invoice Message: ', 'woomp') . $invoice_message;
-			$order->add_order_note($invoiceNote);
+			$invoiceNote     = __( '<b>Ecpay invalid invoice result</b>', 'woomp' ) . __( '<br>Invoice Number: ', 'woomp' ) . $invoice_number . __( '<br>Invoice Message: ', 'woomp' ) . $invoice_message;
+			$order->add_order_note( $invoiceNote );
 
-			if (!empty($sMsg)) {
-				$order->add_order_note($sMsg);
+			if ( ! empty( $sMsg ) ) {
+				$order->add_order_note( $sMsg );
 			}
 
-			if (isset($return_info['RtnCode']) && $return_info['RtnCode'] == 1) {
+			if ( isset( $return_info['RtnCode'] ) && $return_info['RtnCode'] == 1 ) {
 
-				if (empty($totalSuccessTimes)) {
+				if ( empty( $totalSuccessTimes ) ) {
 					$_ecpay_invoice_stauts = '_ecpay_invoice_status';     // 欄位名稱 記錄狀態
 					$_ecpay_invoice_number = '_ecpay_invoice_number';     // 欄位名稱 記錄發票號碼
 				} else {
@@ -315,28 +317,27 @@ class EcpayInvoiceHandler
 				}
 
 				// 異動已經開立發票的狀態 1.已經開立 0.尚未開立
-				$order->update_meta_data($_ecpay_invoice_stauts, 0);
+				$order->update_meta_data( $_ecpay_invoice_stauts, 0 );
 
 				// 清除發票號碼
-				$order->update_meta_data($_ecpay_invoice_number, '');
+				$order->update_meta_data( $_ecpay_invoice_number, '' );
 
 				$order->save();
 			}
 
-			return __('Invalid Ecpay invoice successful!', 'woomp');
+			return __( 'Invalid Ecpay invoice successful!', 'woomp' );
 		} else {
-			return __('Invalid Ecpay invoice error!', 'woomp');
+			return __( 'Invalid Ecpay invoice error!', 'woomp' );
 		}
 	}
 
 	/**
 	 * 取得 API 資料
 	 */
-	private function get_api_key()
-	{
+	private function get_api_key() {
 		$api_data = array();
 
-		if (wc_string_to_bool(get_option('wc_woomp_ecpay_invoice_testmode_enabled'))) {
+		if ( wc_string_to_bool( get_option( 'wc_woomp_ecpay_invoice_testmode_enabled' ) ) ) {
 			$api_data['request_url_issue']   = 'https://einvoice-stage.ecpay.com.tw/Invoice/Issue';
 			$api_data['request_url_invalid'] = 'https://einvoice-stage.ecpay.com.tw/Invoice/IssueInvalid';
 			$api_data['merchant_id']         = '2000132';
@@ -346,14 +347,14 @@ class EcpayInvoiceHandler
 			$api_data['request_url_issue']   = 'https://einvoice.ecpay.com.tw/Invoice/Issue';
 			$api_data['request_url_invalid'] = 'https://einvoice.ecpay.com.tw/Invoice/IssueInvalid';
 
-			if (get_option('RY_WEI_ecpay_MerchantID') && get_option('RY_WEI_ecpay_HashKey') && get_option('RY_WEI_ecpay_HashIV')) {
-				$api_data['merchant_id'] = get_option('RY_WEI_ecpay_MerchantID');
-				$api_data['hashkey']     = get_option('RY_WEI_ecpay_HashKey');
-				$api_data['hashiv']      = get_option('RY_WEI_ecpay_HashIV');
+			if ( get_option( 'RY_WEI_ecpay_MerchantID' ) && get_option( 'RY_WEI_ecpay_HashKey' ) && get_option( 'RY_WEI_ecpay_HashIV' ) ) {
+				$api_data['merchant_id'] = get_option( 'RY_WEI_ecpay_MerchantID' );
+				$api_data['hashkey']     = get_option( 'RY_WEI_ecpay_HashKey' );
+				$api_data['hashiv']      = get_option( 'RY_WEI_ecpay_HashIV' );
 			} else {
-				$api_data['merchant_id'] = get_option('wc_woomp_ecpay_invoice_merchant_id');
-				$api_data['hashkey']     = get_option('wc_woomp_ecpay_invoice_hashkey');
-				$api_data['hashiv']      = get_option('wc_woomp_ecpay_invoice_hashiv');
+				$api_data['merchant_id'] = get_option( 'wc_woomp_ecpay_invoice_merchant_id' );
+				$api_data['hashkey']     = get_option( 'wc_woomp_ecpay_invoice_hashkey' );
+				$api_data['hashiv']      = get_option( 'wc_woomp_ecpay_invoice_hashiv' );
 			}
 		}
 
