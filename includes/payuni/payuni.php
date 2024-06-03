d02d7333c892bba5a5fdd84cac104b5b66e7ca90
J7 <j7.dev.gg@outlook.com>
2024-06-03 17:54:15 +0800

    feat: 如果是訂閱收款，就隱藏"新增付款方式" checkbox

--- includes/payuni/payuni.php
+++ includes/payuni/payuni.php
@@ -1,9 +1,9 @@
 <?php
 
-defined('ABSPATH') || exit;
-define('PAYUNI_PLUGIN_URL', plugin_dir_url(__FILE__));
-define('PAYUNI_PLUGIN_DIR', plugin_dir_path(__FILE__));
-define('PAYUNI_PLUGIN_BASENAME', plugin_basename(__FILE__));
+defined( 'ABSPATH' ) || exit;
+define( 'PAYUNI_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'PAYUNI_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'PAYUNI_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
 
 /**
  * Autoload
@@ -11,11 +11,11 @@ define('PAYUNI_PLUGIN_BASENAME', plugin_basename(__FILE__));
 add_action(
 	'plugins_loaded',
 	function () {
-		if (wc_string_to_bool(get_option('wc_woomp_enabled_payuni_gateway')) || wc_string_to_bool(get_option('wc_woomp_enabled_payuni_shipping'))) {
-			\A7\autoload(WOOMP_PLUGIN_DIR . 'includes/payuni/src');
+		if ( wc_string_to_bool( get_option( 'wc_woomp_enabled_payuni_gateway' ) ) || wc_string_to_bool( get_option( 'wc_woomp_enabled_payuni_shipping' ) ) ) {
+			\A7\autoload( WOOMP_PLUGIN_DIR . 'includes/payuni/src' );
 
-			if (!class_exists('WC_Payment_Gateway')) {
-				wp_die('WC_Payment_Gateway not found');
+			if ( ! class_exists( 'WC_Payment_Gateway' ) ) {
+				wp_die( 'WC_Payment_Gateway not found' );
 			}
 			\PAYUNI\APIs\Payment::init();
 		}
@@ -25,18 +25,33 @@ add_action(
 add_action(
 	'wp_enqueue_scripts',
 	function () {
-		wp_enqueue_script('card-mask', 'https://cdnjs.cloudflare.com/ajax/libs/imask/3.4.0/imask.min.js', array(), '1.0.0', true);
-		wp_enqueue_style('card', PAYUNI_PLUGIN_URL . 'assets/card.css', array(), '1.0.7');
-		wp_register_script('card', PAYUNI_PLUGIN_URL . 'assets/card.js', array('jquery'), '1.5.6', true);
+		wp_enqueue_script( 'card-mask', 'https://cdnjs.cloudflare.com/ajax/libs/imask/3.4.0/imask.min.js', array(), '1.0.0', true );
+		wp_enqueue_style( 'card', PAYUNI_PLUGIN_URL . 'assets/card.css', array(), '1.0.7' );
+		wp_register_script( 'card', PAYUNI_PLUGIN_URL . 'assets/card.js', array( 'jquery' ), '1.5.6', true );
 		wp_localize_script(
 			'card',
 			'card_params',
 			array(
-				'ajax_url'   => admin_url('admin-ajax.php'),
-				'ajax_nonce' => wp_create_nonce('payuni_card_change'),
+				'ajax_url'   => admin_url( 'admin-ajax.php' ),
+				'ajax_nonce' => wp_create_nonce( 'payuni_card_change' ),
 				'user_id'    => get_current_user_id(),
 			)
 		);
-		wp_enqueue_script('card');
+		wp_enqueue_script( 'card' );
 	}
+);
+
+\add_filter(
+	'woocommerce_payment_gateway_save_new_payment_method_option_html',
+	function ( $html, $payment_gateway ) {
+
+		$payment_gateway_id = $payment_gateway->id;
+		if ( 'payuni-credit-subscription' === $payment_gateway_id ) {
+			return '';
+		}
+
+		return $html;
+	},
+	90,
+	2
 );
