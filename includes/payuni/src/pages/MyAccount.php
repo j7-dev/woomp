8cbe30aef9a25721276e7cd6880d3968922389d9
J7 <j7.dev.gg@gmail.com>
2024-07-01 15:48:23 +0800

    fix: 移除修改預設付款方式會修改上層訂閱付款方式 #42

--- includes/payuni/src/pages/MyAccount.php
+++ includes/payuni/src/pages/MyAccount.php
@@ -2,35 +2,36 @@
 
 namespace PAYUNI\Pages;
 
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
 
-class MyAccount
-{
+class MyAccount {
 	/**
 	 * Initialize class and add hooks
 	 *
 	 * @return void
 	 */
-	public static function init()
-	{
+	public static function init() {
 		$class = new self();
-		\add_filter('woocommerce_my_account_my_orders_actions', array($class, 'change_customer_order_action'), 10, 2);
-		\add_action('wp_enqueue_scripts', array($class, 'enqueue_my_account_script'));
-		\add_action('woocommerce_payment_token_set_default', array($class, 'update_credit_hash'), 30, 2);
+		\add_filter( 'woocommerce_my_account_my_orders_actions', array(
+			$class,
+			'change_customer_order_action'
+		), 10, 2 );
+		\add_action( 'wp_enqueue_scripts', array( $class, 'enqueue_my_account_script' ) );
+//		\add_action('woocommerce_payment_token_set_default', array($class, 'update_credit_hash'), 30, 2);
 	}
-	public function change_customer_order_action($actions, $order)
-	{
+
+	public function change_customer_order_action( $actions, $order ) {
 		$order_status   = $order->get_status();
 		$payment_method = $order->get_payment_method();
 
-		switch ($order_status) {
+		switch ( $order_status ) {
 			case 'failed':
-				if ('linepay' !== $payment_method) {
+				if ( 'linepay' !== $payment_method ) {
 					break;
 				}
 
-				unset($actions['pay']);
-				unset($actions['cancel']);
+				unset( $actions['pay'] );
+				unset( $actions['cancel'] );
 
 				break;
 		}
@@ -40,52 +41,57 @@ class MyAccount
 		return $actions;
 	}
 
-	public function enqueue_my_account_script()
-	{
-		if (is_account_page()) {
-			wp_register_script('payuni_my_account_script', WOOMP_PLUGIN_URL . 'includes/payuni/assets/my-account.js', array('jquery'), '1.1.8', true);
+	public function enqueue_my_account_script() {
+		if ( is_account_page() ) {
+			wp_register_script( 'payuni_my_account_script', WOOMP_PLUGIN_URL . 'includes/payuni/assets/my-account.js', array( 'jquery' ), '1.1.8', true );
 			wp_localize_script(
 				'payuni_my_account_script',
 				'payuni_my_account_script_params',
 				array(
-					'ajax_url'   => admin_url('admin-ajax.php'),
-					'ajax_nonce' => wp_create_nonce('payuni_refund'),
+					'ajax_url'   => admin_url( 'admin-ajax.php' ),
+					'ajax_nonce' => wp_create_nonce( 'payuni_refund' ),
 					'user_id'    => get_current_user_id(),
 				)
 			);
-			wp_enqueue_script('payuni_my_account_script');
+			wp_enqueue_script( 'payuni_my_account_script' );
 		}
 	}
+
 	/**
 	 * 當用戶設定信用卡為預設時，更新訂閱的第一筆訂單中的信用卡資訊
 	 * @see https://github.com/j7-dev/woomp/issues/37
 	 *
-	 * @param  integer              $token_id
-	 * @param  \WC_PAYMENT_TOKEN_CC $token
+	 * @param integer $token_id
+	 * @param \WC_PAYMENT_TOKEN_CC $token
 	 *
 	 * @return void
 	 */
-	public function update_credit_hash(int $token_id, \WC_PAYMENT_TOKEN_CC $token): void
-	{
-		if (!class_exists('WC_Subscriptions')) return;
+	public function update_credit_hash( int $token_id, \WC_PAYMENT_TOKEN_CC $token ): void {
+		if ( ! class_exists( 'WC_Subscriptions' ) ) {
+			return;
+		}
 
 		$user_id = get_current_user_id();
-		if (!$user_id) return;
+		if ( ! $user_id ) {
+			return;
+		}
 		// find all subscriptions for this user
-		$subscriptions = wcs_get_users_subscriptions($user_id);
+		$subscriptions = wcs_get_users_subscriptions( $user_id );
 		// get subscription post parent id
 		$parent_order_ids = [];
-		foreach ($subscriptions as $subscription) {
-			$subscription_id = $subscription->get_id();
-			$parent_id = wp_get_post_parent_id($subscription_id);
+		foreach ( $subscriptions as $subscription ) {
+			$subscription_id    = $subscription->get_id();
+			$parent_id          = wp_get_post_parent_id( $subscription_id );
 			$parent_order_ids[] = $parent_id;
 		}
 
-		foreach ($parent_order_ids as $order_id) {
-			$order = wc_get_order($order_id);
-			if (!$order) continue;
-			$order->update_meta_data('_payuni_card_hash', $token->get_token());
-			$order->update_meta_data('_payuni_card_number', $token->get_last4());
+		foreach ( $parent_order_ids as $order_id ) {
+			$order = wc_get_order( $order_id );
+			if ( ! $order ) {
+				continue;
+			}
+			$order->update_meta_data( '_payuni_card_hash', $token->get_token() );
+			$order->update_meta_data( '_payuni_card_number', $token->get_last4() );
 
 			//	_payuni_resp_card_bank  	_payuni_resp_trade_no 	_payuni_resp_message _payuni_resp_status
 			$order->save();
