56f6f40802fe5e48cbfd444759370a9ef006b55f
J7 <j7.dev.gg@gmail.com>
2024-08-15 12:48:04 +0800

    feat: [PAYNOW] 重寫 JS

--- includes/paynow-einvoice/public/js/paynow-einvoice-public.js
+++ includes/paynow-einvoice/public/js/paynow-einvoice-public.js
@@ -1,84 +1,79 @@
-(function ( $ ) {
+(function ($) {
 	'use strict';
 
-	$( document ).ready(
-		function () {
-			var issue_type   = $( '#paynow_ei_issue_type' ).val();
-			var carrier_type = $( '#paynow_ei_carrier_type' ).val();
-			change_fields( issue_type, carrier_type );
+	class PaynowEinvoiceHandler {
+
+		fieldsEnum = {
+			issueType: 'paynow_ei_issue_type',
+			carrierType: 'paynow_ei_carrier_type'
+		};
+
+		_paynow_ei_issue_type = 'b2c';
+		_paynow_ei_carrier_type = '';
+
+		constructor() {
+			this.attachEvents();
+			this.paynow_ei_issue_type = 'b2c';
 		}
-	);
 
-	$( document ).on(
-		'change',
-		'#paynow_ei_issue_type',
-		function () {
+		set paynow_ei_issue_type(value) {
+			this._paynow_ei_issue_type = value;
+			this.paynow_ei_carrier_type = ''; // 每次改變時，將載具類型改為預設的雲端載具
 
-			var issue_type = $( '#paynow_ei_issue_type' ).val();
-			if (issue_type == 'b2b') {
-
-				$( '#paynow-ei-carrier-type' ).hide();
-				change_fields( issue_type,'ei_carrier_type_ubn' );
-
-			} else if (issue_type == 'b2c') {
-
-				$( '#paynow-ei-carrier-type' ).show();
-				change_fields( issue_type, 'ei_carrier_type_mobile_code' );
-
+			if ('b2b' === value) {
+				// 如果是開公司發票，則自動帶入公司名稱
+				$('#paynow_ei_buyer_name').val($('#billing_company').val());
 			} else {
-				$( '#paynow-ei-carrier-type' ).hide();
-				change_fields( issue_type, 'ei_carrier_type_donate' );
+				// 如果是開個人發票，則自動帶入購買者姓名
+				$('#paynow_ei_buyer_name').val(`${$('#billing_last_name').val()}${$('#billing_first_name').val()}`);
 			}
-
-		}
-	);
-
-	$( document ).on(
-		'change',
-		'#paynow_ei_carrier_type',
-		function () {
-
-			var issue_type = $( '#paynow_ei_issue_type' ).val();
-			change_fields( issue_type, $( this ).val() );
-
-		}
-	);
-
-	function change_fields( issue_type, carrier_type ) {
-		console.log( issue_type, carrier_type );
-		var $c_title     = $( '#paynow-ei-company-title' );
-		var $ubn         = $( '#paynow-ei-ubn' );
-		var $carrier_num = $( '#paynow-ei-carrier-num' );
-		var $donate_org  = $( '#paynow-ei-org' );
-
-		if ( carrier_type == 'ei_carrier_type_ubn' ) {
-			$c_title.show();
-			$ubn.show();
-			$carrier_num.hide();
-			$donate_org.hide();
 		}
 
-		if ( carrier_type == 'ei_carrier_type_mobile_code' ) {
-			$c_title.hide();
-			$ubn.hide();
-			$carrier_num.show();
-			$donate_org.hide();
+		set paynow_ei_carrier_type(value) {
+			this._paynow_ei_carrier_type = value;
+			this.changeFields();
 		}
 
-		if ( carrier_type == 'ei_carrier_type_cdc_code' ) {
-			$c_title.hide();
-			$ubn.hide();
-			$carrier_num.show();
-			$donate_org.hide();
+		changeFields() {
+			const issue_type = this._paynow_ei_issue_type;
+			const carrier_type = this._paynow_ei_carrier_type;
+
+			const displayCarrierType = issue_type === 'b2c' ? 'show' : 'hide';
+			$('#paynow-ei-carrier-type')?.[displayCarrierType](); // 隱藏/顯示整個載具類型 show() / hide()
+
+			// 只有開公司發票時才需要顯示填寫，其他時候 buyer name 自動帶入購買者姓名
+			const displayCompanyInfo = issue_type === 'b2b' ? 'show' : 'hide';
+			$('#paynow-ei-buyer-name')?.[displayCompanyInfo]();
+			$('#paynow-ei-ubn')?.[displayCompanyInfo]();
+
+			// 個人發票  且  非雲端載具才需要填寫"載具"欄位
+			const displayCarrierNum = (issue_type === 'b2c' && carrier_type !== '') ? 'show' : 'hide';
+			$('#paynow-ei-carrier-num')?.[displayCarrierNum]();
+
+
+			// 只有發票類型為捐贈時才需要顯示填寫"捐贈給"欄位
+			const displayDonateOrg = (issue_type === 'donate') ? 'show' : 'hide';
+			$('#paynow-ei-org')?.[displayDonateOrg]();
 		}
 
-		if ( carrier_type == 'ei_carrier_type_donate' ) {
-			$c_title.hide();
-			$ubn.hide();
-			$carrier_num.hide();
-			$donate_org.show();
+		attachEvents() {
+			Object.values(this.fieldsEnum).forEach(
+				(field) => {
+					$(`#${field}`).on(
+						'change',
+						() => {
+							this[`${field}`] = $(`#${field}`).val();
+						}
+					);
+				}
+			);
 		}
-
 	}
 
-})( jQuery );
+	$(document).ready(
+		function () {
+			new PaynowEinvoiceHandler();
+		}
+	);
+
+})(jQuery);
