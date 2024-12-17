fac7576e32d959766b76dda74f5184fee14c3a7e
J7 <j7.dev.gg@gmail.com>
2024-12-17 19:06:27 +0800

    feat: 修改 debug log

--- debug.php
+++ debug.php
@@ -1,38 +1,42 @@
 <?php
+/**
+ * Log class
+ * 方便印出 Error Log
+ *
+ * @package J7\WpUtils
+ */
+
 namespace J7\WpUtils\Classes;
 
 if (!class_exists('J7\WpUtils\Classes\ErrorLog')) {
-	class ErrorLog {
+	/**
+	 * Log class
+	 */
+	abstract class ErrorLog {
+
 		/**
 		 * Log a message.
 		 *
-		 * @param string $message The message to log.
+		 * @param mixed  $message The message to log.
 		 * @param string $level The log level.
+		 * @param string $context The context of the message.
 		 *
 		 * @return void
 		 */
-		protected static function log( $message, $level = 'info' ): void {
-
-			switch ( $level ) {
-				case 'info':
-					$emoji = '📘 ';
-					break;
-				case 'error':
-					$emoji = '❌ ';
-					break;
-				case 'debug':
-					$emoji = '🐛 ';
-					break;
-				default:
-					$emoji = '';
-					break;
-			}
+		protected static function log( $message, string $level = 'info', ?string $context = '' ): void {
+			$emoji = match ( $level ) {
+				'info' => '📘 ',
+				'error' => '❌ ',
+				'debug' => '🐛 ',
+				default => ' ',
+			};
 
 			$formatted_message = sprintf(
-			'[%1$s%2$s] %3$s',
+			'[%1$s%2$s] %3$s %4$s',
 			$emoji,
 			strtoupper( $level ),
-			$message
+			$context,
+			self::stringify( $message )
 			);
 
 			if ( defined( 'ABSPATH' ) ) {
@@ -44,15 +48,54 @@ if (!class_exists('J7\WpUtils\Classes\ErrorLog')) {
 				error_log( $formatted_message );
 			}
 		}
+
+		/**
+		 * Stringify the message.
+		 *
+		 * @param mixed $message The message to stringify.
+		 *
+		 * @return string The stringified message.
+		 */
+		public static function stringify( $message ): string {
+			ob_start();
+			var_dump($message);
+			return ob_get_clean();
+		}
+
 		/**
 		 * Log a info message.
 		 *
-		 * @param string $message The message to log.
+		 * @param mixed  $message The message to log.
+		 * @param string $context The context of the message.
 		 *
 		 * @return void
 		 */
-		public static function info( $message ): void {
-			self::log( $message, 'info' );
+		public static function info( $message, ?string $context = '' ): void {
+			self::log( $message, 'info', $context );
+		}
+
+		/**
+		 * Log a error message.
+		 *
+		 * @param mixed  $message The message to log.
+		 * @param string $context The context of the message.
+		 *
+		 * @return void
+		 */
+		public static function error( $message, ?string $context = '' ): void {
+			self::log( $message, 'error', $context );
+		}
+
+		/**
+		 * Log a debug message.
+		 *
+		 * @param mixed  $message The message to log.
+		 * @param string $context The context of the message.
+		 *
+		 * @return void
+		 */
+		public static function debug( $message, ?string $context = '' ): void {
+			self::log( $message, 'debug', $context );
 		}
 	}
 }
